d4993c726f52c8b974ab63564b2e8c1c5b772f79
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-18 23:05:27 -0800

    Update FolderPluginLoader.php

--- FolderPluginLoader/src/FolderPluginLoader/FolderPluginLoader.php
+++ FolderPluginLoader/src/FolderPluginLoader/FolderPluginLoader.php
@@ -27,117 +27,126 @@ use pocketmine\Server;
 use pocketmine\utils\MainLogger;
 use pocketmine\utils\TextFormat;
 
-class FolderPluginLoader implements PluginLoader{
+class FolderPluginLoader implements PluginLoader
+{
 
-	/** @var Server */
-	private $server;
+    /** @var Server */
+    private $server;
 
-	/**
-	 * @param Server $server
-	 */
-	public function __construct(Server $server){
-		$this->server = $server;
-	}
+    /**
+     * @param Server $server
+     */
+    public function __construct(Server $server)
+    {
+        $this->server = $server;
+    }
 
-	/**
-	 * Loads the plugin contained in $file
-	 *
-	 * @param string $file
-	 *
-	 * @return Plugin
-	 */
-	public function loadPlugin($file){
-		if(is_dir($file) and file_exists($file . "/plugin.yml") and file_exists($file . "/src/")){
-			if(($description = $this->getPluginDescription($file)) instanceof PluginDescription){
-				MainLogger::getLogger()->info(TextFormat::LIGHT_PURPLE."Loading source plugin " . $description->getFullName());
-				$dataFolder = dirname($file) . DIRECTORY_SEPARATOR . $description->getName();
-				if(file_exists($dataFolder) and !is_dir($dataFolder)){
-					trigger_error("Projected dataFolder '" . $dataFolder . "' for " . $description->getName() . " exists and is not a directory", E_USER_WARNING);
+    /**
+     * Loads the plugin contained in $file
+     *
+     * @param string $file
+     *
+     * @return Plugin
+     */
+    public function loadPlugin($file)
+    {
+        if (is_dir($file) && file_exists($file . "/plugin.yml") && file_exists($file . "/src/")) {
+            if (($description = $this->getPluginDescription($file)) instanceof PluginDescription) {
+                MainLogger::getLogger()->info(TextFormat::LIGHT_PURPLE."Loading source plugin " . $description->getFullName());
+                $dataFolder = dirname($file) . DIRECTORY_SEPARATOR . $description->getName();
+                if (file_exists($dataFolder) && !is_dir($dataFolder)) {
+                    trigger_error("Projected dataFolder '" . $dataFolder . "' for " . $description->getName() . " exists and is not a directory", E_USER_WARNING);
 
-					return null;
-				}
+                    return null;
+                }
 
 
-				$className = $description->getMain();
-				$this->server->getLoader()->addPath($file . "/src");
+                $className = $description->getMain();
+                $this->server->getLoader()->addPath($file . "/src");
 
-				if(class_exists($className, true)){
-					$plugin = new $className();
-					$this->initPlugin($plugin, $description, $dataFolder, $file);
+                if (class_exists($className, true)) {
+                    $plugin = new $className();
+                    $this->initPlugin($plugin, $description, $dataFolder, $file);
 
-					return $plugin;
-				}else{
-					trigger_error("Couldn't load source plugin " . $description->getName() . ": main class not found", E_USER_WARNING);
+                    return $plugin;
+                } else {
+                    trigger_error("Couldn't load source plugin " . $description->getName() . ": main class not found", E_USER_WARNING);
 
-					return null;
-				}
-			}
-		}
+                    return null;
+                }
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * Gets the PluginDescription from the file
-	 *
-	 * @param string $file
-	 *
-	 * @return PluginDescription
-	 */
-	public function getPluginDescription($file){
-		if(is_dir($file) and file_exists($file . "/plugin.yml")){
-			$yaml = @file_get_contents($file . "/plugin.yml");
-			if($yaml != ""){
-				return new PluginDescription($yaml);
-			}
-		}
+    /**
+     * Gets the PluginDescription from the file
+     *
+     * @param string $file
+     *
+     * @return PluginDescription
+     */
+    public function getPluginDescription($file)
+    {
+        if (is_dir($file) && file_exists($file . "/plugin.yml")) {
+            $yaml = @file_get_contents($file . "/plugin.yml");
+            if ($yaml != "") {
+                return new PluginDescription($yaml);
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * Returns the filename patterns that this loader accepts
-	 *
-	 * @return array
-	 */
-	public function getPluginFilters(){
-		return "/[^\\.]/";
-	}
+    /**
+     * Returns the filename patterns that this loader accepts
+     *
+     * @return array
+     */
+    public function getPluginFilters()
+    {
+        return "/[^\\.]/";
+    }
 
-	/**
-	 * @param PluginBase        $plugin
-	 * @param PluginDescription $description
-	 * @param string            $dataFolder
-	 * @param string            $file
-	 */
-	private function initPlugin(PluginBase $plugin, PluginDescription $description, $dataFolder, $file){
-		$plugin->init($this, $this->server, $description, $dataFolder, $file);
-		$plugin->onLoad();
-	}
+    /**
+     * @param PluginBase        $plugin
+     * @param PluginDescription $description
+     * @param string            $dataFolder
+     * @param string            $file
+     */
+    private function initPlugin(PluginBase $plugin, PluginDescription $description, $dataFolder, $file)
+    {
+        $plugin->init($this, $this->server, $description, $dataFolder, $file);
+        $plugin->onLoad();
+    }
 
-	/**
-	 * @param Plugin $plugin
-	 */
-	public function enablePlugin(Plugin $plugin){
-		if($plugin instanceof PluginBase and !$plugin->isEnabled()){
-			MainLogger::getLogger()->info("Enabling " . $plugin->getDescription()->getFullName());
+    /**
+     * @param Plugin $plugin
+     */
+    public function enablePlugin(Plugin $plugin)
+    {
+        if ($plugin instanceof PluginBase && !$plugin->isEnabled()) {
+            MainLogger::getLogger()->info("Enabling " . $plugin->getDescription()->getFullName());
 
-			$plugin->setEnabled(true);
+            $plugin->setEnabled(true);
 
-			Server::getInstance()->getPluginManager()->callEvent(new PluginEnableEvent($plugin));
-		}
-	}
+            Server::getInstance()->getPluginManager()->callEvent(new PluginEnableEvent($plugin));
+        }
+    }
 
-	/**
-	 * @param Plugin $plugin
-	 */
-	public function disablePlugin(Plugin $plugin){
-		if($plugin instanceof PluginBase and $plugin->isEnabled()){
-			MainLogger::getLogger()->info("Disabling " . $plugin->getDescription()->getFullName());
+    /**
+     * @param Plugin $plugin
+     */
+    public function disablePlugin(Plugin $plugin)
+    {
+        if ($plugin instanceof PluginBase and $plugin->isEnabled()) {
+            MainLogger::getLogger()->info("Disabling " . $plugin->getDescription()->getFullName());
 
-			Server::getInstance()->getPluginManager()->callEvent(new PluginDisableEvent($plugin));
+            Server::getInstance()->getPluginManager()->callEvent(new PluginDisableEvent($plugin));
 
-			$plugin->setEnabled(false);
-		}
-	}
+            $plugin->setEnabled(false);
+        }
+    }
+
 }
